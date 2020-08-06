f0488f6b804c3966151497bab27a635c5721d41c
HunterGreen <hunter.green95@hotmail.com>
2020-08-06 14:19:36 +0930

    Questions 5 & 6

--- Uni/Software Engineering/Y4S1/C++/pracs/prac1_week2/main.cpp
+++ Uni/Software Engineering/Y4S1/C++/pracs/prac1_week2/main.cpp
@@ -5,46 +5,62 @@
 
 int main()
 {
-  //Game introduction
-  std::cout << "Welcome to Software Development with C++" << std::endl;
-  std::cout << "Would you like to play a game? (y/n) ";
-  char input;
-  std::cin >> input;
 
+    std::cout << "Welcome to Software Development with C++" << std::endl;
+    bool choice = 1;
+    int highScore = 11;
+    while(choice){
+      //Game introduction
 
-  if (tolower(input) == 'y') {
-    std::cout << "Try to guess how many fingers I'm holding up (0 - 10, inclusive): ";
-    // The below line creates and seeds a mersenne twister random number generator with the current time
-    std::mt19937 mtRand{uint32_t(time(nullptr))};
-    int fingers = mtRand() % 10;
+      std::cout << "Would you like to play a game? (y/n) ";
+      char input;
 
-    //Applies the players input to a variable
-    int guess;
-    std::cin >> guess;
-    int guessCount = 1;
+      std::cin >> input;
 
-    //Loop while the guess is incorrect
-    while (guess != fingers) {
-        std::cout << "guessCount: " << guessCount << std::endl;
-      if (guess < fingers) {
-        std::cout << "More than that." << std::endl;
-      } else if (guess > fingers) {
-        std::cout << "Less than that." << std::endl;
-      } else {
-        break;
+      if (tolower(input) == 'y') {
+        std::cout << "Try to guess how many fingers I'm holding up (0 - 10, inclusive): ";
+        // The below line creates and seeds a mersenne twister random number generator with the current time
+        std::mt19937 mtRand{uint32_t(time(nullptr))};
+        int fingers = mtRand() % 10;
+
+        //Applies the players input to a variable
+        int guess;
+        std::cin >> guess;
+        int guessCount = 1;
+
+        //Loop while the guess is incorrect
+        while (guess != fingers) {
+          if (guess < fingers) {
+            std::cout << "More than that." << std::endl;
+          } else if (guess > fingers) {
+            std::cout << "Less than that." << std::endl;
+          } else {
+            break;
+          }
+          std::cout << "Have another guess: ";
+          std::cin.clear();
+          std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+          std::cin >> guess;
+          std::cout << "You entered: " << guess << std::endl;
+          ++guessCount;
+        }
+
+        //Exit loop
+        std::cout << "You guessed correctly, "
+                  << ((guessCount == 1) ? "on the first try!":
+                      (guessCount < 5) ? "well done." : "pity it took so long.")
+                  << std::endl;
+        if(guessCount < highScore){
+            highScore = guessCount;
+             std::cout << "Wow new high score! :" << highScore << std::endl;
+        }
+        else{
+             std::cout << "Your high score is :" << highScore << std::endl;
+        }
+
       }
-      std::cout << "Have another guess: ";
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-      std::cin >> guess;
-      std::cout << "You entered: " << guess << std::endl;
-      ++guessCount;
+      else{
+        choice = 0;
+      }
     }
-
-    //Exit loop
-    std::cout << "You guessed correctly, "
-              << ((guessCount == 1) ? "on the first try!":
-                  (guessCount < 5) ? "well done." : "pity it took so long.")
-              << std::endl;
-  }
 }

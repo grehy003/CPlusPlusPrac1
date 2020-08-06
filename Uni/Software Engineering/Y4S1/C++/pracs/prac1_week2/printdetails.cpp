6894316b4065c82d8a0da6adfc4277827eba9c36
HunterGreen <hunter.green95@hotmail.com>
2020-08-06 14:21:03 +0930

    Print details file added

---
+++ Uni/Software Engineering/Y4S1/C++/pracs/prac1_week2/printdetails.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+  printMyDetails();
+}
+
+void printMyDetails() {
+  std::cout << "Name: Matt Selway" << std::endl
+            << "Email: matt.selway@unisa.edu.au" << std::endl;
+}

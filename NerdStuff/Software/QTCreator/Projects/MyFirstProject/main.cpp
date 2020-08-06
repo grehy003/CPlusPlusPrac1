3fc5d98e0fc0e0d6612e628d21a0e5b908a3a144
HunterGreen <hunter.green95@hotmail.com>
2020-08-06 14:03:37 +0930

    Removed pre input, false guess message and counter

--- NerdStuff/Software/QTCreator/Projects/MyFirstProject/main.cpp
+++ NerdStuff/Software/QTCreator/Projects/MyFirstProject/main.cpp
@@ -1,9 +1,64 @@
-#include <iostream>
-
+#include <cstdio>
 using namespace std;
+
+int linkedInCases(int input);
 
 int main()
 {
-    cout << "Hello World!" << endl;
+//    int x = 707;
+//    int y = 42;
+
+//    printf("x is %d\n", x);
+//    printf("y is %d\n", y);
+//    printf("The lesser value is %d\n", x < y ? x : y);
+
+
+   //WHILE AND DO
+    /* continue : ends execution of current iteration within loop
+     * break : exits loop completely
+     * do{} while{} : condition is tested at end of loop iteration
+     */
+
+    int array[] = {1, 2, 3, 4, 5};
+    int i = 0;
+    while (i < 5) {
+        printf("array element %d equals %d\n", i, array[i]);
+        i++;
+    }
+
+//    linkedInCases(2);
+
     return 0;
 }
+
+
+//CASE/SWITCH STATEMENTS
+int linkedInCases (int input) {
+    const int iONE = 1;
+    const int iTWO = 2;
+    const int iTHREE = 3;
+
+    const int iFOUR = 4;
+
+    //Cases require constants
+    switch (input) {
+    case iONE:
+        puts("one");
+        break;
+    case iTWO:
+        puts("two");
+        break;
+    case iTHREE:
+        puts("three");
+        break;
+    case iFOUR:
+        puts("four");
+        break;
+    default:
+        puts("default");
+        break;
+
+        return 0;
+    }
+}
+
--- Uni/Software Engineering/Y4S1/C++/pracs/prac1_week2/main.cpp
+++ Uni/Software Engineering/Y4S1/C++/pracs/prac1_week2/main.cpp
@@ -1,8 +1,50 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <cctype>
+#include <ctime>
+#include <random>
 
-int main(int argc, char *argv[])
+int main()
 {
-    QCoreApplication a(argc, argv);
+  //Game introduction
+  std::cout << "Welcome to Software Development with C++" << std::endl;
+  std::cout << "Would you like to play a game? (y/n) ";
+  char input;
+  std::cin >> input;
 
-    return a.exec();
+
+  if (tolower(input) == 'y') {
+    std::cout << "Try to guess how many fingers I'm holding up (0 - 10, inclusive): ";
+    // The below line creates and seeds a mersenne twister random number generator with the current time
+    std::mt19937 mtRand{uint32_t(time(nullptr))};
+    int fingers = mtRand() % 10;
+
+    //Applies the players input to a variable
+    int guess;
+    std::cin >> guess;
+    int guessCount = 1;
+
+    //Loop while the guess is incorrect
+    while (guess != fingers) {
+        std::cout << "guessCount: " << guessCount << std::endl;
+      if (guess < fingers) {
+        std::cout << "More than that." << std::endl;
+      } else if (guess > fingers) {
+        std::cout << "Less than that." << std::endl;
+      } else {
+        break;
+      }
+      std::cout << "Have another guess: ";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      std::cin >> guess;
+      std::cout << "You entered: " << guess << std::endl;
+      ++guessCount;
+    }
+
+    //Exit loop
+    std::cout << "You guessed correctly, "
+              << ((guessCount == 1) ? "on the first try!":
+                  (guessCount < 5) ? "well done." : "pity it took so long.")
+              << std::endl;
+  }
 }

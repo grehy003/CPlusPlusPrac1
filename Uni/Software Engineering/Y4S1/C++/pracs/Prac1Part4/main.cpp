88f64653b9fca9c77a42e451322e31a57de87084
HunterGreen <hunter.green95@hotmail.com>
2020-08-07 11:09:39 +0930

    Cleaning up lastIndexCalc method

--- Uni/Software Engineering/Y4S1/C++/pracs/Prac1Part4/main.cpp
+++ Uni/Software Engineering/Y4S1/C++/pracs/Prac1Part4/main.cpp
@@ -6,19 +6,75 @@
 #include <thread>
 
 unsigned int getRandom();
+int promptAnswerSort(char);
+char promptAnswer();
+std::vector<int> generateNumbersVector();
+void printVectorElements(std::vector<int> numbers);
+int sumVector(std::vector<int> numbers);
+int lastIndexCalc(int,std::vector<int>);
+void lastIndexProcessCheck(int,int);
 
 int main()
 {
     std::cout << "This program demonstrates C++ basic syntax" << std::endl;
 
     // TODO: implement according to the instructions
-    std::cout << "Would you like to begin: (y/n)" << std::endl;
+    promptAnswerSort(promptAnswer());
+
+    std::vector<int> numbers{};
+    numbers = generateNumbersVector();
+    printVectorElements(numbers);
+
+    int sumOfVector = sumVector(numbers);
+    std::cout << "The summed value is: " << sumVector(numbers) << std::endl;
+
+    int lastIndex = lastIndexCalc(sumOfVector, numbers);
+    std::cout << "The lastIndex value is: " << lastIndex << std::endl;
+    lastIndexProcessCheck(lastIndex, numbers.size());
+    return 0;
+
+}
+
+int lastIndexCalc(int sumVector,std::vector<int> numbers){
+    int numberIterator;
+    for(int i = 0; i < numbers.size(); i++){
+        if(sumVector != numbers.at(i)){
+            sumVector -= numbers.at(i);
+        }
+        else{
+            numberIterator = i - 1;
+            break;
+        }
+    }
+    return numberIterator > 0 ? numberIterator -1 : abs(numberIterator -1) ;
+}
+
+void lastIndexProcessCheck(int lastIndex, int numbersSize){
+    if(lastIndex == numbersSize -1){
+        std::cout << "Processed all numbers" << std::endl;
+    }
+    else if(lastIndex > (numbersSize -1) / 2 ){
+        std::cout << "Almost made it all the way" << std::endl;
+    }
+    else{
+         std::cout << "Didn't even get half way" << std::endl;
+    }
+}
 
 
-    char beginAnswer;
-    std::cin >> beginAnswer;
 
-    switch(beginAnswer){
+int sumVector(std::vector<int> numbers){
+    int numberSum = 0;
+    int numberIterator = 0;
+    while (numberSum % 2 == 0 && numberIterator < numbers.size() - 1) {
+        numberSum += numbers.at(numberIterator);
+        numberIterator++;
+    }
+    return numberSum;
+}
+
+int promptAnswerSort(char answer){
+    switch(answer){
     case 'y':
         std::cout << "You said Yes, continuing execution." << std::endl;
         break;
@@ -28,52 +84,21 @@ int main()
     case 'n':
         std::cout << "You said No, quitting now." << std::endl;
         return 0;
-        break;
     case 'N':
         std::cout << "You said No, quitting now." << std::endl;
         return 0;
-        break;
     default:
         std::cout << "You entered an invalid option. Terminating with error" << std::endl;
         return 1;
     }
+    return 2;
+}
 
-    std::vector<int> numbers{};
-    for(int i = 0; i < 10; i++){
-        numbers.push_back(getRandom() % 100);
-    }
-
-    std::cout << "The numbers are: " << std::endl;
-
-    for(int i : numbers){
-        std::cout << i << std::endl;
-    }
-
-    int numberSum = 0;
-    int numberIterator = 0;
-    while (numberSum % 2 == 0 && numberIterator < numbers.size() - 1) {
-        numberSum += numbers.at(numberIterator);
-        numberIterator++;
-    }
-    std::cout << "The summed value is: " << numberSum << std::endl;
-
-    //Ternary syntax: Condition        ? True              : False     ;
-    int lastIndex = numberIterator > 0 ? numberIterator -1 : abs(numberIterator -1) ;
-
-    std::cout << "The lastIndex value is: " << lastIndex << std::endl;
-    return 0;
-
-
-    if(lastIndex == numbers.size() -1){
-        std::cout << "Processed all numbers" << std::endl;
-    }
-    else if(lastIndex > (numbers.size() -1) / 2 ){
-        std::cout << "Almost made it all the way" << std::endl;
-    }
-    else{
-         std::cout << "Didn't even get half way" << std::endl;
-    }
-
+char promptAnswer(){
+    std::cout << "Would you like to begin: (y/n)" << std::endl;
+    char beginAnswer;
+    std::cin >> beginAnswer;
+    return beginAnswer;
 }
 
 /**
@@ -89,3 +114,20 @@ unsigned int getRandom() {
     static std::mt19937 mtRand{uint32_t(time(nullptr))};
     return mtRand();
 }
+
+std::vector<int> generateNumbersVector(){
+    std::vector<int> numbers{};
+    for(int i = 0; i < 10; i++){
+        numbers.push_back(getRandom() % 100);
+    }
+    return numbers;
+}
+
+void printVectorElements(std::vector<int> numbers){
+    std::cout << "The numbers are: " << std::endl;
+    for(int i : numbers){
+        std::cout << i << std::endl;
+    }
+}
+
+

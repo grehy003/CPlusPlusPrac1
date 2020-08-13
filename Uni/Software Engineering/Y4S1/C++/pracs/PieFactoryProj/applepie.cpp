9fc9287bfb826ac24003c3583a520d219b54773c
Hunter <grehy003@mymail.unisa.edu.au>
2020-08-13 18:16:47 +0930

    Classes to represent pies created
    ApplePie class, header, source files added
    ApricotPie class, header, source files added
    RaspberryPie class, header, source files added

---
+++ Uni/Software Engineering/Y4S1/C++/pracs/PieFactoryProj/applepie.cpp
@@ -0,0 +1,6 @@
+#include "applepie.h"
+
+ApplePie::ApplePie()
+{
+
+}
---
+++ Uni/Software Engineering/Y4S1/C++/pracs/PieFactoryProj/raspberrypie.h
@@ -0,0 +1,11 @@
+#ifndef RASPBERRYPIE_H
+#define RASPBERRYPIE_H
+
+
+class RaspberryPie
+{
+public:
+    RaspberryPie();
+};
+
+#endif // RASPBERRYPIE_H

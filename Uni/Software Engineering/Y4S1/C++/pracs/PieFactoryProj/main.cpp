06ffd09c59cff2457834412dc134e0098e471dd7
Hunter <grehy003@mymail.unisa.edu.au>
2020-08-13 18:48:33 +0930

    Main function prompts
    Main now prompts user for a type of pie, then prints the given
    description for the pie.

--- Uni/Software Engineering/Y4S1/C++/pracs/PieFactoryProj/main.cpp
+++ Uni/Software Engineering/Y4S1/C++/pracs/PieFactoryProj/main.cpp
@@ -1,8 +1,32 @@
 #include <QCoreApplication>
+#include <iostream>
+#include <string>
+#include <piefactory.h>
 
-int main(int argc, char *argv[])
+int main()
 {
-    QCoreApplication a(argc, argv);
+    std::cout << "Which type of pie would you like? Enter 1 for Apple, 2 For Apricot or 3 for Raspberry ";
+    int choice;
+    std::cin >> choice;
+    PieFactory factory;
 
-    return a.exec();
+    switch(choice) {
+    case 1:{
+            std::cout << factory.createApplePie().Description();
+            break;
+        }
+    case 2:{
+            std::cout << factory.createApricotPie().Description();
+            break;
+        }
+    case 3: {
+            std::cout << factory.createRaspberryPie().Description();
+            break;
+        }
+    default:{
+            std::cout << "Please enter a number from 1 to 3";
+        }
+    }
+
+    return 0;
 }

6fb5c6d4b7d7884622442aacb3ef01953a11ea2d
Hunter <grehy003@mymail.unisa.edu.au>
2020-08-19 20:50:33 +0930

    Doxygen file created, Project name changed to Doxygen Installation Project

---
+++ Uni/Software Engineering/Y4S1/C++/pracs/DoxygenInstallation/main.cpp
@@ -0,0 +1,14 @@
+#include <QCoreApplication>
+
+
+
+int main()
+{
+    /**
+     * @brief main Executes the example application.
+     * @return 0 if application exited successfully, non-zero otherwise
+     */
+
+
+   return 0;
+}

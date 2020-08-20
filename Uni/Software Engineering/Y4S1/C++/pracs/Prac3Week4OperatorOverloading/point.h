174a864135fd8144b96e5fc5368c8419c33f6b9e
Hunter <grehy003@mymail.unisa.edu.au>
2020-08-20 21:26:49 +0930

    Point Declaration
    private coordinates, setters & getters

--- Uni/Software Engineering/Y4S1/C++/pracs/Prac3Week4OperatorOverloading/point.h
+++ Uni/Software Engineering/Y4S1/C++/pracs/Prac3Week4OperatorOverloading/point.h
@@ -6,6 +6,13 @@ class Point
 {
 public:
     Point();
+    double x() const;
+    double y() const;
+    void setX(double &x);
+    void setY(double &y);
+private:
+    double _x;
+    double _y;
 };
 
 #endif // POINT_H
